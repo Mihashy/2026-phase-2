5ae364cf09fbe8209096c3cb5c0f19974c3eb436
Yoshinori Hattori <hattori.index.js@gmail.com>
2026-08-15 20:11:40 +0900

    Revert "fix: envをgitの管理から外す"

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from "react"
-import { Link } from "react-router"
-import { Button } from "~/components/ui/button"
 
 export default function TopPage() {
 	const [message, setMessage] = useState<string | null>(null)
@@ -11,18 +9,8 @@ export default function TopPage() {
 	}, [])
 
 	return (
-		<div className="flex flex-col justify-center items-center">
+		<div>
 			<h1>トップページ</h1>
-			<Button variant="brand" size="xl" className="rounded-2xl w-[150px]" asChild>
-					<Link to="/auth/register">
-						アカウント登録
-					</Link>
-				</Button>
-			<Button variant="brand" size="xl" className="rounded-2xl w-[150px]" asChild>
-					<Link to="/app">
-						ホーム画面
-					</Link>
-				</Button>
 			<div>{message}</div>
 		</div>
 	)
