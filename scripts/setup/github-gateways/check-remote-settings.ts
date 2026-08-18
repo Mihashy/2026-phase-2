c045dcf941cdb6d2952c3b808beaac8af07c252a
hattori-expertbook <hattori.index.js@gmail.com>
2026-08-18 19:03:48 +0900

    feat: upstreamも同様に作成

--- scripts/setup/github-gateways/check-remote-settings.ts
+++ scripts/setup/github-gateways/check-remote-settings.ts
@@ -1,6 +1,6 @@
 import { $ } from "bun"
 import { logError, logInfo } from "../utils/logger"
-import { GITHUB_REPO_NAME } from "./constants"
+import { GITHUB_REPO_NAME, GITHUB_WINC_OWNER_ID } from "./constants"
 
 function extractRepoInfoFromUrl(
 	url: string,
@@ -52,6 +52,50 @@ export async function checkRemoteOriginSettings(userName: string) {
 
 	logInfo([
 		"リモート「origin」が正しく設定されていることを確認しました。",
+		`url : ${url}`,
+		`owner : ${owner}`,
+		`repo : ${repo}`,
+	])
+}
+
+export async function checkRemoteUpstreamSettings() {
+	const shellResult = await $`git config --get "remote.upstream.url"`
+		.quiet()
+		.nothrow()
+
+	if (shellResult.exitCode !== 0) {
+		logError("リモート「upstream」のURLの取得に失敗しました。")
+		process.exit(1)
+	}
+
+	const url = shellResult.text().trim()
+
+	const extractResult = extractRepoInfoFromUrl(url)
+	if (extractResult === null) {
+		logError("リモート「upstream」の解析に失敗しました。")
+		process.exit(1)
+	}
+
+	const { owner, repo } = extractResult
+
+	if (owner !== GITHUB_WINC_OWNER_ID) {
+		logError([
+			`リモート「upstream」の owner が「${GITHUB_WINC_OWNER_ID}」と一致していません。`,
+			`owner : ${owner}`,
+		])
+		process.exit(1)
+	}
+
+	if (repo !== GITHUB_REPO_NAME) {
+		logError([
+			`リモート「upstream」のリポジトリ名が「${GITHUB_REPO_NAME}」と一致していません。`,
+			`repo : ${repo}`,
+		])
+		process.exit(1)
+	}
+
+	logInfo([
+		"リモート「upstream」が正しく設定されていることを確認しました。",
 		`url : ${url}`,
 		`owner : ${owner}`,
 		`repo : ${repo}`,
--- scripts/setup/index.ts
+++ scripts/setup/index.ts
@@ -1,5 +1,3 @@
-import { checkRemoteOriginSettings } from "./github-gateways/check-remote-settings"
-import { GITHUB_WINC_OWNER_ID } from "./github-gateways/constants"
 import { ensureGitHubCliAvailable } from "./github-gateways/ensure-cli-available"
 import { getGitHubUserName } from "./github-gateways/get-user-name"
 
@@ -7,7 +5,7 @@ async function main() {
 	await ensureGitHubCliAvailable()
 	const userName = await getGitHubUserName()
 	// await checkRemoteOriginSettings(userName)
-	await checkRemoteOriginSettings(GITHUB_WINC_OWNER_ID)
+	// await checkRemoteUpstreamSettings()
 }
 
 main()
