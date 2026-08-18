bd6caaa5fcb1ef1d159915b014a5a62691fa7467
hattori-expertbook <hattori.index.js@gmail.com>
2026-08-18 20:07:12 +0900

    feat: ラベル作成

--- scripts/setup/github-gateways/constants.ts
+++ scripts/setup/github-gateways/constants.ts
@@ -2,3 +2,21 @@ export const GITHUB_WINC_OWNER_ID = "winc1980"
 export const GITHUB_REPO_NAME = "2026-phase-2"
 
 export const MENTORS = ["Hattori-1104"]
+
+export const LABEL_NAMES = ["EASY", "NORMAL", "HARD", "EXTREME"] as const
+
+type LabelName = (typeof LABEL_NAMES)[number]
+
+export const LABEL_COLORS: Record<LabelName, string> = {
+	EASY: "07C051",
+	NORMAL: "05ADCA",
+	HARD: "EE6B0E",
+	EXTREME: "834DE7",
+}
+
+export const LABEL_DESCRIPTIONS: Record<LabelName, string> = {
+	EASY: "基礎的な難易度の課題",
+	NORMAL: "標準的、発展的な難易度の課題",
+	HARD: "発展的で他分野の知識が必要な課題",
+	EXTREME: "専門的で複数レイヤーにまたがる技能が求められる課題",
+}
--- scripts/setup/index.ts
+++ scripts/setup/index.ts
@@ -1,5 +1,10 @@
 import { applyRuleset } from "./github-gateways/apply-ruleset"
-import { setRemoteRepoDefault } from "./github-gateways/check-remote-settings"
+import {
+	checkRemoteOriginSettings,
+	checkRemoteUpstreamSettings,
+	setRemoteRepoDefault,
+} from "./github-gateways/check-remote-settings"
+import { createLabels } from "./github-gateways/create-labels"
 import { ensureGitHubCliAvailable } from "./github-gateways/ensure-cli-available"
 import { getGitHubUserName } from "./github-gateways/get-user-name"
 import { inviteMentor } from "./github-gateways/invite-mentor"
@@ -8,12 +13,13 @@ import { makeCodeownersFile } from "./github-gateways/make-codeowners-file"
 async function main() {
 	await ensureGitHubCliAvailable()
 	const userName = await getGitHubUserName()
-	// await checkRemoteOriginSettings(userName)
-	// await checkRemoteUpstreamSettings()
+	await checkRemoteOriginSettings(userName)
+	await checkRemoteUpstreamSettings()
 	await setRemoteRepoDefault()
 	await applyRuleset(userName)
 	await inviteMentor(userName)
 	await makeCodeownersFile()
+	await createLabels(userName)
 }
 
 main()
