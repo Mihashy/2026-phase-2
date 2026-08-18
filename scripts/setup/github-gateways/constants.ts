3cfb5bd26ad0f0dcfa73d182db220da88e304cd3
hattori-expertbook <hattori.index.js@gmail.com>
2026-08-18 18:57:07 +0900

    feat: originが正しく設定されていることをチェック

---
+++ scripts/setup/github-gateways/constants.ts
@@ -0,0 +1,2 @@
+export const GITHUB_WINC_OWNER_ID = "winc1980"
+export const GITHUB_REPO_NAME = "2026-phase-2"
--- scripts/setup/index.ts
+++ scripts/setup/index.ts
@@ -1,9 +1,13 @@
+import { checkRemoteOriginSettings } from "./github-gateways/check-remote-settings"
+import { GITHUB_WINC_OWNER_ID } from "./github-gateways/constants"
 import { ensureGitHubCliAvailable } from "./github-gateways/ensure-cli-available"
 import { getGitHubUserName } from "./github-gateways/get-user-name"
 
 async function main() {
 	await ensureGitHubCliAvailable()
 	const userName = await getGitHubUserName()
+	// await checkRemoteOriginSettings(userName)
+	await checkRemoteOriginSettings(GITHUB_WINC_OWNER_ID)
 }
 
 main()
